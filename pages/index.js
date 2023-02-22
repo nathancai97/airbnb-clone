f1a4c3a8a951568bbb6d6b16317dafe724b62897
Nathan Cai <nathan97cai@gmail.com>
2023-02-21 23:06:44 -0500

    adding banner

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
@@ -8,9 +9,14 @@ export default function Home() {
         <title>Airbnb</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Header/>
-      {/* Banner */}
+      <Header />
+      <Banner />
 
+      <main className="max-w-7xl mx-auto px-8 sm:px-16">
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+        </section>
+      </main>
     </div>
   );
-};
+}
